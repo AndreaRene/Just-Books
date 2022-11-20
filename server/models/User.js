0f23a76fd20134809a0449775046f5e7758a8ac2
Andrea <andrea@interhact.net>
2022-11-20 12:32:59 -0600

    reverting user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,9 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
+// import schema from Book.js
+const bookSchema = require('./Book');
+
 const userSchema = new Schema(
   {
     username: {
@@ -18,13 +21,16 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
-
-    savedBooks: [
-      {
-        type: Schema.Types.ObjectID,
-      }
-    ],
-  });
+    // set savedBooks to be an array of data that adheres to the bookSchema
+    savedBooks: [bookSchema],
+  },
+  // set this to use virtual below
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 // hash user password
 userSchema.pre('save', async function (next) {
@@ -40,6 +46,11 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.isCorrectPassword = async function (password) {
   return bcrypt.compare(password, this.password);
 };
+
+// when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
+userSchema.virtual('bookCount').get(function () {
+  return this.savedBooks.length;
+});
 
 const User = model('User', userSchema);
 
