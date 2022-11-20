17e3df43c097f3f32a4121fde78fc2d9faf4a247
Andrea <andrea@interhact.net>
2022-11-20 16:03:50 -0600

    troubleshooting syntax error

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,16 +1,20 @@
 import { gql } from '@apollo/client';
 
 export const GET_ME = gql`
-	query me($username: String) {
-		me(username: $username) {
-			username
-			email
-			savedBooks {
-				authors
-				title
-				description
-				bookId
-			}
-		}
-	}
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
 `;
