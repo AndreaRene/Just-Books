22f2ad582c475ab3a5786e131bc7e9921d554dcb
Andrea <andrea@interhact.net>
2022-11-20 12:39:56 -0600

    setup login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,5 +16,25 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
+        // Create user
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError(
+                    'No user found with this email address'
+                );
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
     }
 }
