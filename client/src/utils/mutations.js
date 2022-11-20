137ade525fb156c287d3ba09df4174f963088641
Andrea <andrea@interhact.net>
2022-11-20 15:01:15 -0600

    removed useeffect hook. added usequery hook in savedbooks

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,9 +1,9 @@
 import { gql } from '@apollo/client';
 
-export const CREATE_USER = gql``;
+export const ADD_USER = gql``;
 
 export const LOGIN = gql``;
 
 export const SAVE_BOOK = gql``;
 
-export const DELETE_BOOK = gql``;
+export const REMOVE_BOOK = gql``;
